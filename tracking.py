833de889523035a835dac37e9c5c2f98cc4abb96
unknownpgr <unknownpgr@gmail.com>
2020-11-17 21:41:45 +0900

    Add edges on cube

--- tracking.py
+++ tracking.py
@@ -4,9 +4,9 @@ import math
 
 cap = cv2.VideoCapture(0)
 
-theta = 63*math.pi/180
+theta = 71*math.pi/180
 dist = 26.5
-bias = 13.25
+bias = 125
 
 cx = 0
 cy = 0
@@ -17,16 +17,27 @@ h, w, _ = img.shape
 SCR_W = 380
 SCR_H = 265
 
+# 화면 세로 폭 22.9cm
+# 화면 가로 폭 36.65cm
+# 화면 위쪽 경계에서 카메라 중심까지 0.65cm
 
-def track_dot(img, color=30, epsilon=10, thresh=128):
-    bound_upper = img[:, :, 0] < (color+epsilon)
-    bound_lower = img[:, :, 0] > (color-epsilon)
-    bound_satur = img[:, :, 1] > thresh
+
+def track_dot(img, color=30, epsilon=10, thresh=160):
+    '''
+    Get the position of color dot from hsv type imge
+    '''
+
+    bound_upper = img[:, :, 0] < (color+epsilon)  # Upper hue bound
+    bound_lower = img[:, :, 0] > (color-epsilon)  # Lower hue bound
+    bound_satur = img[:, :, 1] > thresh  # Saturation threshold
     bound = bound_upper & bound_lower & bound_satur
+
+    # Get detected points
     ys, xs = np.nonzero(bound)
 
     N = len(ys)
 
+    # Calculate median point
     if N > 0:
         xs = sorted(xs)
         ys = sorted(ys)
@@ -36,8 +47,10 @@ def track_dot(img, color=30, epsilon=10, thresh=128):
 
 
 def get_cam_pos(img_w, img_h, theta, dist, cx, cy, bias):
-    # Calculate relative camera position
-    # Range = [-1,1]
+    '''
+    Calculate relative camera position
+    Range = [-1,1]
+    '''
     crx = cx*2/img_w-1
     cry = (cy*2-img_h)/img_w
 
@@ -49,13 +62,22 @@ def get_cam_pos(img_w, img_h, theta, dist, cx, cy, bias):
 
 
 def project(cx, cy, cz, ox, oy, oz):
+    '''
+    Project given point to screen
+    [cx,cy,cz] is the position of the camera.
+    [ox,oy,oz] is the position of the point.
+    '''
     return [
         (cx*oz-ox*cz)/(oz-cz),
         (cy*oz-oy*cz)/(oz-cz)
     ]
 
 
-def get_cube(x, y, z, d=1):
+def get_cube(x, y, z, d=2):
+    '''
+    Get the points and edges of cube
+    '''
+    d /= 2
     return [
         [x+d, y+d, z+d],
         [x+d, y+d, z-d],
@@ -65,6 +87,19 @@ def get_cube(x, y, z, d=1):
         [x-d, y+d, z-d],
         [x-d, y-d, z+d],
         [x-d, y-d, z-d]
+    ], [
+        [0, 1],
+        [2, 3],
+        [4, 5],
+        [6, 7],
+        [0, 2],
+        [1, 3],
+        [4, 6],
+        [5, 7],
+        [0, 4],
+        [1, 5],
+        [2, 6],
+        [3, 7]
     ]
 
 
@@ -75,7 +110,7 @@ def get_screen_pos(x, y, scale, scr_w, scr_h):
     return x, y
 
 
-cube = get_cube(-5, 2, -10, 2)
+points, edges = get_cube(0, 0, -10, 4)
 back = cv2.resize(cv2.imread('./room.jpg'), (SCR_W, SCR_H))//2
 cv2.namedWindow('Video', cv2.WINDOW_FREERATIO)
 while True:
@@ -93,10 +128,12 @@ while True:
 
     screen = np.copy(back)
 
-    for dot in cube:
-        x, y = project(rx, ry, -dist, dot[0], dot[1], dot[2])
-        x, y = get_screen_pos(x, y, 10, SCR_W, SCR_H)
-        screen[y-1:y+1, x-1:x+1] = 255
+    for edge in edges:
+        x1, y1 = get_screen_pos(
+            *project(rx, ry, -dist, *points[edge[0]]), 10, SCR_W, SCR_H)
+        x2, y2 = get_screen_pos(
+            *project(rx, ry, -dist, *points[edge[1]]), 10, SCR_W, SCR_H)
+        screen = cv2.line(screen, (x1, y1), (x2, y2), (255, 255, 255), 1)
 
     cv2.imshow('Video', screen)
     cv2.waitKey(1)
